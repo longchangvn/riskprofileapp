d7f7613d0c364a2cddf07534f2b0f841980a64b1
Divv <github@turtlezombie.com>
2018-12-06 11:47:56 +1100

    PUT method added to save a new profile or replace an existing one

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,17 +1,20 @@
 # builtin
 import os
+from sys import stdout
 import glob
 import logging
 import logging.config
 
 # installed
 import yaml
+import hiyapyco
 from pprint import pprint, pformat
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
 
 
 log_format = "%(asctime)s.%(msecs)03d | %(levelname)s | elapsed=%(relativeCreated)d ms loc=%(module)s.%(funcName)s:%(lineno)d\n%(message)s"
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format=log_format,
                     datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger('riskeasy')
@@ -21,7 +24,7 @@ logger = logging.getLogger('riskeasy')
 app = Flask(__name__)
 
 # Flask routes
-@app.route('/surveys')
+@app.route('/surveys', methods=['GET'])
 def list_surveys():
     survey_names = []
     for key in SURVEYS:
@@ -31,13 +34,13 @@ def list_surveys():
     return jsonify(survey_names)
 
 
-@app.route('/surveys/<survey_name>')
+@app.route('/surveys/<survey_name>', methods=['GET'])
 def get_survey_questions(survey_name):
     # logger.info(f"PhysicalSafety Questions: {get_survey_questions(surveys["PhysicalSafety"])}")
     return jsonify(SURVEYS[survey_name])
 
 
-@app.route('/profiles')
+@app.route('/profiles', methods=['GET'])
 def list_profiles():
     profiles = get_local_profile_list()
     logger.info(f"{len(profiles)} profiles found locally")
@@ -45,27 +48,35 @@ def list_profiles():
     return jsonify(profiles)
 
 
-@app.route('/profiles/<ndis_id>')
-def get_risk_profile(ndis_id):
-    profile = load_profile(ndis_id)
+@app.route('/profiles/<ndis_id>', methods=['GET', 'PUT'])
+def handle_profile(ndis_id):
+    if request.method == 'GET':
+        return jsonify(load_profile(ndis_id))
+    elif request.method == 'PUT':
+        payload = request.get_json()
+        logger.debug(f"PUT received for /profiles/{ndis_id}")
+        logger.debug(f"Request:\n{payload}")
+        save_profile(payload)
+        msg = f"Profile for {ndis_id} saved."
+        return jsonify(msg), 201
 
-    return jsonify(profile)
 
+# @app.route('/profiles/')
 
 
 # Utilities
 def get_working_dir():
     current_file_dir = os.path.dirname(os.path.realpath(__file__))
-    working_dir = os.path.join(current_file_dir, "../../data")
-    logger.debug(f"current_file_dir: {current_file_dir}")
-    logger.debug(f"working_dir: {working_dir}")
+    working_dir = os.path.realpath(os.path.join(current_file_dir, "../../data"))
+    logger.debug(f"main.py path: {current_file_dir}\main.py")
+    logger.debug(f"WORKING_DIR: {working_dir}")
 
     return working_dir
 
 
 def read_survey_config():
     survey_file = os.path.join(WORKING_DIR, "surveys.yaml")
-    logger.debug(f"survey_file: {survey_file}")
+    logger.info(f"Survey file: {survey_file}")
 
     with open(survey_file, 'r') as f:
         return yaml.load(f)
@@ -90,8 +101,49 @@ def load_profile(ndis_id):
         return yaml.load(f)
 
 
+def save_profile(json_payload):
+    ndis_id = json_payload['ndis_id']
+    logger.debug(f"Converting JSON to YAML")
+    recvd_yaml = yaml.dump(json_payload)
+    logger.debug(f"**** Received YAML ****:\n{recvd_yaml}")
+
+    filename = f"{ndis_id}.yaml"
+    filepath = os.path.join(WORKING_DIR, "profiles", filename)
+    logger.debug(f"Writing profile to {filepath} for {ndis_id}")
+    with open(filepath, 'w') as f:
+        f.write(recvd_yaml)
+    logger.info(f"Saved profile successfully.")
+
+
+def merge_profile(json_payload):
+    ndis_id = json_payload['ndis_id']
+    logger.debug(f"Converting JSON to YAML")
+    recvd_yaml = yaml.dump(json_payload)
+    logger.debug(f"**** Received YAML ****:\n{recvd_yaml}")
+
+    filename = f"{ndis_id}.yaml"
+    filepath = os.path.join(WORKING_DIR, "profiles", filename)
+    if os.path.isfile(filepath):
+        logger.info("Merging into existing profile")
+
+        with open(filepath, 'r') as f:
+            existing_yaml = f.read()
+        logger.debug(f"**** Existing YAML ****:\n{existing_yaml}")
+
+        merged_obj = hiyapyco.load([existing_yaml, recvd_yaml],
+                                    method=hiyapyco.METHOD_MERGE,
+                                    loglevel=logging.INFO,
+                                    mergelists=False)
+        merged_yaml = hiyapyco.dump(merged_obj)
+        logger.debug(f"**** Merged YAML ****:\n{merged_yaml}")
+
+        logger.debug(f"Overwriting profile at {filepath} for {ndis_id}")
+        with open(filepath, 'w') as f:
+            f.write(merged_yaml)
+
+
 # init common stuff
 with app.app_context():
     WORKING_DIR = get_working_dir()
     SURVEYS = read_survey_config()
-    logger.info("App loaded and ready.")
+    logger.info("READY: Webserver loaded.")
--- readme.md
+++ readme.md
@@ -48,11 +48,97 @@ e.g. <http://127.0.0.1:5000/profiles>
 ["123456","984161"]
 ```
 
-### /profiles/<ndis_id>
+### /profiles/<ndis_id> GET
 Returns the contents of the selected profile on the local disk (/path/to/project/data/profiles/${ndis_id}.yaml)
 
 e.g. <http://127.0.0.1:5000/profiles/123456>
 
 ```python
-{"name":"Bob Jones","ndis_id":123456,"surveys":{"PhysicalSafety":{"PhysicalRisk1":[{"Comments":"Customer lives in a bouncy castle\n","Date":"Tue, 04 Dec 2018 00:00:00 GMT","Level":"Low"},{"Comments":"Bouncy castle deflated.\nCustomer still on soft grass\n","Date":"Wed, 05 Dec 2018 00:00:00 GMT","Level":"Med"}],"PhysicalRisk2":[{"Date":"Tue, 04 Dec 2018 00:00:00 GMT","Level":3}]},"SubstanceUse":{"SubstanceRisk2":[{"Comments":"Disorientated. Lighter found on ground.\nPossibly the reason why bouncy castle was damaged and deflated\n","Date":"Wed, 05 Dec 2018 00:00:00 GMT","Level":4}]}}}
+{  
+  "name":"Bob Jones",
+  "ndis_id":123456,
+  "surveys":{  
+    "PhysicalSafety":{  
+      "PhysicalRisk1":[  
+        {  
+          "Comments":"Customer lives in a bouncy castle\n",
+          "Date":"Tue, 04 Dec 2018 00:00:00 GMT",
+          "Level":"Low"
+        },
+        {  
+          "Comments":"Bouncy castle deflated.\nCustomer still on soft grass\n",
+          "Date":"Wed, 05 Dec 2018 00:00:00 GMT",
+          "Level":"Med"
+        }
+      ],
+      "PhysicalRisk2":[  
+        {  
+          "Date":"Tue, 04 Dec 2018 00:00:00 GMT",
+          "Level":3
+        }
+      ]
+    },
+    "SubstanceUse":{  
+      "SubstanceRisk2":[  
+        {  
+          "Comments":"Disorientated. Lighter found on ground.\nPossibly the reason why bouncy castle was damaged and deflated\n",
+          "Date":"Wed, 05 Dec 2018 00:00:00 GMT",
+          "Level":4
+        }
+      ]
+    }
+  }
+}
+```
+
+### /profiles/<ndis_id> PUT
+Replaces the existing profile, with an updated one
+
+e.g. <http://127.0.0.1:5000/profiles/123456 PUT>
+
+```python
+{  
+  "ndis_id": 123456,
+  "name":"Bob Jones",
+  "surveys":{  
+    "PhysicalSafety":{  
+      "PhysicalRisk1":[  
+        {  
+          "Comments":"Customer lives in a bouncy castle",
+          "Date":"Tue, 04 Dec 2018 00:00:00 GMT",
+          "Level":"Low"
+        },
+        {  
+          "Comments":"Bouncy castle deflated.\nCustomer still on soft grass",
+          "Date":"Wed, 05 Dec 2018 00:00:00 GMT",
+          "Level":"Med"
+        }
+      ],
+      "PhysicalRisk2":[  
+        {  
+          "Date":"Tue, 04 Dec 2018 00:00:00 GMT",
+          "Level":3
+        }
+      ]
+    },
+    "SubstanceUse":{  
+      "SubstanceRisk2":[  
+        {  
+          "Comments":"Disorientated. Lighter found on ground.\nPossibly the reason why bouncy castle was damaged and deflated",
+          "Date":"Wed, 05 Dec 2018 00:00:00 GMT",
+          "Level":4
+        }
+      ]
+    }
+  },
+    "MedicalHistory": {
+      "MedicalRisk2": [
+        {
+          "Date": "2018-12-09",
+          "Level": "High",
+          "Comments": "Even worse influenza."
+        }
+      ]
+    }
+}
 ```
