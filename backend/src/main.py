1d20f119413a7ecdfeafb13a8a1ffb3f8cbae445
Divv <github@turtlezombie.com>
2018-12-07 14:39:04 +1100

    change log level

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,7 @@ from flask_mail import Mail, Message
 
 
 log_format = "%(asctime)s.%(msecs)03d | %(levelname)s | elapsed=%(relativeCreated)d ms loc=%(module)s.%(funcName)s:%(lineno)d\n%(message)s"
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format=log_format,
                     datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger('riskeasy')
